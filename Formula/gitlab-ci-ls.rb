9c9556d4107dbed973597a163ea9cda43abed4d8
Ales Brelih <ales.brelih@3fs.si>
2024-03-30 20:45:38 +0100

    feat: added gitlabci formula

---
+++ Formula/gitlab-ci-ls.rb
@@ -0,0 +1,26 @@
+class GitlabCiLs < Formula
+  desc "Gitlab CI Language Server"
+  homepage "https://github.com/alesbrelih/gitlab-ci-ls"
+  url "https://github.com/alesbrelih/gitlab-ci-ls/archive/refs/tags/0.5.0.zip"
+  sha256 "ae83f3deb73979688b4aa99af54af0143cb8fc2eb059211e25f7637191bc0d89"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test gitlab-ci-ls`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "which gitlab-ci-ls"
+  end
+end
