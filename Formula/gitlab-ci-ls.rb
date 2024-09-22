afd98b57bf1636584c3cc0e2bfbc4f99907dd5c5
Ales Brelih <ales.brelih@3fs.si>
2024-09-22 14:52:55 +0200

    core: 0.21.2

--- Formula/gitlab-ci-ls.rb
+++ Formula/gitlab-ci-ls.rb
@@ -1,8 +1,8 @@
 class GitlabCiLs < Formula
   desc "Gitlab CI Language Server"
   homepage "https://github.com/alesbrelih/gitlab-ci-ls"
-  url "https://github.com/alesbrelih/gitlab-ci-ls/archive/refs/tags/0.21.1.tar.gz"
-  sha256 "fb8d870eda127bcad325dba3e11227b353d4374fdc853fc2ca8a664546edda39"
+  url "https://github.com/alesbrelih/gitlab-ci-ls/archive/refs/tags/0.21.2.tar.gz"
+  sha256 "0998538bd43b75d68221d7f8e72c3f12adf2eb53e16d9e7419fa74534804e1c9"
   license "MIT"
 
   depends_on "rust" => :build
