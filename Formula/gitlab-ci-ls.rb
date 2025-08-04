83354cd3b26999978894a3feb0d560e70b7e369e
GitHub Action <action@github.com>
2025-08-04 10:44:11 +0000

    core: version 1.1.6

--- Formula/gitlab-ci-ls.rb
+++ Formula/gitlab-ci-ls.rb
@@ -1,8 +1,8 @@
 class GitlabCiLs < Formula
   desc "Gitlab CI Language Server"
   homepage "https://github.com/alesbrelih/gitlab-ci-ls"
-  url "https://github.com/alesbrelih/gitlab-ci-ls/archive/refs/tags/1.1.2.tar.gz"
-  sha256 "a5665fa5ed68fa8cd09cd8dd07c21170bb36fb50aae3994e79f92c24d287d11a"
+  url "https://github.com/alesbrelih/gitlab-ci-ls/archive/refs/tags/1.1.6.tar.gz"
+  sha256 "94be324a9a476db033a260a16893d0a2d96870dd928b648a3f38e34e92c983b1"
   license "MIT"
 
   depends_on "rust" => :build
